01c252ba23ee8e9e3111fd0ff70da60c258fcea1
kszeto20 <kszeto20@stuy.edu>
2021-11-04 11:21:45 -0400

    changed to take command line inputs again

--- main.c
+++ main.c
@@ -5,16 +5,11 @@ int main(int argc, char *argv[]) {
 
     DIR *d;
     struct dirent *directDent;
-    d = opendir("./");
-    long size = 0;
 
     if (argc > 1) {
       d = opendir(argv[1]);
-      if (d == NULL) {
+      if (errno != 0) {
         printf("There was an error opening the directory: %s", strerror(errno));
-        return 0;
-      }
-      else {
         printf("Please give a valid path: \n");
         char newPath[500];
 
@@ -22,37 +17,35 @@ int main(int argc, char *argv[]) {
         read(STDIN_FILENO, newPath, sizeof(newPath));
 
         d = opendir(argv[1]);
-        if (d == NULL) {
+        if (errno != 0) {
           printf("There was an error opening the directory: %s", strerror(errno));
           return 0;
         }
       }
     }
-    else  {
+    long size = 0;
 
-      directDent = readdir(d);
+    directDent = readdir(d);
 
-      printf("Statistics for directory: ./ (Current Directory)\n");
+    printf("Statistics for directory: ./ (Current Directory)\n");
 
-      // readdir = next
-      while (directDent != NULL) {
+    // readdir = next
+    while (directDent != NULL) {
 
-        struct stat sub;
-        stat(directDent->d_name, &sub);
+      struct stat sub;
+      stat(directDent->d_name, &sub);
 
-        if (directDent->d_type == DT_DIR) {
-            printf("%s \t\tis a directory\n", directDent->d_name);
-        }
-        else {
-            printf("%s\n", directDent->d_name);
-        }
-
-        size += sub.st_size;
-        directDent = readdir(d);
-
+      if (directDent->d_type == DT_DIR) {
+          printf("%s \t\tis a directory\n", directDent->d_name);
       }
-    printf("Total Directory Size: %lu Bytes\n", size);
-  }
+      else {
+          printf("%s\n", directDent->d_name);
+      }
 
-    return 0;
+      size += sub.st_size;
+      directDent = readdir(d);
+    }
+  printf("Total Directory Size: %lu Bytes\n", size);
+
+  return 0;
 }
